301e75476ba5ff8182803816218aaecb3a861873
TiNYsx <jiwjirawat@gmail.com>
2025-01-30 16:56:18 +0700

    1.2 indev

---
+++ WelcomeAds/src/main/java/com/tinysx/welcomeads/InventoryStorage.java
@@ -0,0 +1,67 @@
+package com.tinysx.welcomeads;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+public final class InventoryStorage {
+    private final Player player;
+    private final Inventory inventory;
+    static List<InventoryStorage> inventoryStorages = new ArrayList<>();
+
+    public InventoryStorage(Player player) {
+        this.inventory = Bukkit.createInventory(player, 54);
+        this.inventory.setContents(player.getInventory().getContents());
+        this.player = player;
+    }
+
+    public static InventoryStorage createInventoryStorage(Player player) {
+        InventoryStorage storage = new InventoryStorage(player);
+        addInventoryStorage(storage);
+        return storage;
+    }
+
+    private static void addInventoryStorage(InventoryStorage storage) {
+        InventoryStorage.inventoryStorages.add(storage);
+    }
+
+    public static boolean isHaveInventoryStorage(Player player) {
+        for (InventoryStorage storage : InventoryStorage.inventoryStorages) {
+            if (storage.getPlayer().equals(player)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void removeInventoryStorage(Player player) {
+        InventoryStorage storage = getInventoryStorage(player);
+        if (storage != null) {
+            InventoryStorage.inventoryStorages.remove(storage);
+        }
+    }
+
+    public static InventoryStorage getInventoryStorage(Player player) {
+        for (InventoryStorage storage : InventoryStorage.inventoryStorages) {
+            if (storage.getPlayer().equals(player)) {
+                return storage;
+            }
+        }
+        return null;
+    }
+
+    public Inventory getInventory() {
+        return this.inventory;
+    }
+
+    public Player getPlayer() {
+        return this.player;
+    }
+
+    public InventoryStorage clone(Player player) {
+        return new InventoryStorage(player);
+    }
+}
---
+++ WelcomeAds/src/main/java/com/tinysx/welcomeads/Screen.java
@@ -0,0 +1,183 @@
+package com.tinysx.welcomeads;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.logging.Level;
+
+import javax.annotation.Nullable;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.profile.PlayerProfile;
+
+import de.tr7zw.changeme.nbtapi.NBT;
+import de.tr7zw.changeme.nbtapi.iface.ReadWriteNBT;
+import me.clip.placeholderapi.PlaceholderAPI;
+import net.skinsrestorer.api.SkinsRestorer;
+import net.skinsrestorer.api.SkinsRestorerProvider;
+import net.skinsrestorer.api.exception.DataRequestException;
+import net.skinsrestorer.api.property.SkinProperty;
+
+class Screen {
+    private final WelcomeAds plugin = WelcomeAds.getPlugin(WelcomeAds.class);
+    SkinsRestorer skinsRestorerAPI;
+    private final Inventory screenInventory;
+    private final String index;
+    private final String title;
+    private final String background;
+    private final ConfigurationSection itemsection;
+
+    public Screen(String index, Player player) {
+        this.index = index;
+        this.background = plugin.getConfig().getString("background.text");
+        this.title = ChatColor.translateAlternateColorCodes('&', PlaceholderAPI.setPlaceholders(player, "&f" + this.background + "&f" + plugin.getConfig().getString("inventory." + index + ".title")));
+        this.itemsection = plugin.getConfig().getConfigurationSection("inventory." + this.index + ".items");
+        this.screenInventory = Bukkit.createInventory(new WelcomeInventoryHolder(this.index), 54, this.title);
+    }
+
+    public String getIndex() {
+        return this.index;
+    }
+
+    public String getBackground() {
+        return this.background;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public Inventory getScreenInventory(Player player) {
+
+        for (String key : this.itemsection.getKeys(false)) {
+            this.screenInventory.setItem(this.itemsection.getInt(key + ".slot"), getItem(key, player));
+        }
+
+        return this.screenInventory;
+    }
+
+    public ConfigurationSection getItemSection() {
+        return this.itemsection;
+    }
+
+    public ItemStack getItem(String key, Player player) {
+        String headOwner = player.getName();
+
+        String itemName = this.itemsection.getString(key + ".name");
+        String itemMaterial = this.itemsection.getString(key + ".material");
+        int itemModelData = this.itemsection.getInt(key + ".modeldata");
+        List<String> itemLore = this.itemsection.getStringList(key + ".lore");
+
+        if (itemMaterial == null || itemName == null) {
+            return null;
+        }
+
+        if (itemLore.isEmpty()) {
+            itemLore = new ArrayList<>();
+        }
+
+        if (itemMaterial.contains(":")) {
+            String material = itemMaterial.split(":")[0];
+            headOwner = itemMaterial.split(":")[1];
+            itemMaterial = material;
+        }
+
+        ItemStack item = new ItemStack(Material.getMaterial(itemMaterial));
+        if (Material.getMaterial(itemMaterial) == Material.PLAYER_HEAD) {
+            try {
+                this.skinsRestorerAPI = SkinsRestorerProvider.get();
+                SkullMeta meta = (SkullMeta) item.getItemMeta();
+                SkinProperty skinProperty = this.skinsRestorerAPI.getSkinStorage().getPlayerSkin(headOwner, false).orElse(null).getSkinProperty();
+                UUID skinUUID = this.skinsRestorerAPI.getSkinStorage().getPlayerSkin(headOwner, false).orElse(null).getUniqueId();
+                String textures = skinProperty.getValue();
+                PlayerProfile playerProfile = Bukkit.getServer().createPlayerProfile(skinUUID, headOwner);
+                meta.setOwnerProfile(playerProfile);
+                item.setItemMeta(meta);
+                NBT.modifyComponents(item, nbt -> {
+                    ReadWriteNBT profileNbt = nbt.getOrCreateCompound("minecraft:profile");
+                    profileNbt.setUUID("id", skinUUID);
+                    ReadWriteNBT propertiesNbt = profileNbt.getCompoundList("properties").addCompound();
+                    propertiesNbt.setString("name", "textures");
+                    propertiesNbt.setString("value", textures);
+                });
+                
+                ItemMeta skullmeta = item.getItemMeta();
+                if (skullmeta != null) {
+                    skullmeta.setCustomModelData(itemModelData);
+                    skullmeta.setDisplayName(PlaceholderAPI.setPlaceholders(player, ChatColor.translateAlternateColorCodes('&', itemName)));
+                    for (int i = 0; i < itemLore.size(); i++){
+                        itemLore.set(i, PlaceholderAPI.setPlaceholders(player, ChatColor.translateAlternateColorCodes('&', itemLore.get(i))));
+                    }
+                    skullmeta.setLore(itemLore);
+                    item.setItemMeta(skullmeta);
+                }
+            } catch (DataRequestException ex) {
+            }
+        }
+        else{
+            ItemMeta meta = item.getItemMeta();
+            if (meta != null){
+                if (itemModelData >= 0){
+                    meta.setCustomModelData(itemModelData);
+                    item.setItemMeta(meta);
+                    meta.setDisplayName(PlaceholderAPI.setPlaceholders(player, ChatColor.translateAlternateColorCodes('&', itemName)));
+                    for (int i = 0; i < itemLore.size(); i++){
+                        itemLore.set(i, PlaceholderAPI.setPlaceholders(player, ChatColor.translateAlternateColorCodes('&', itemLore.get(i))));
+                    }
+                    meta.setLore(itemLore);
+                    item.setItemMeta(meta);
+                }
+            }   
+        }
+
+        NBT.modify(item, nbt -> {
+            nbt.setString("adsid", index);
+            nbt.setBoolean("welcomeads", true);
+        });
+
+        return item;
+    }
+
+    public void openTo(Player player, @Nullable boolean isClear) {
+        if (this.itemsection == null) {
+            player.sendMessage("§7[§e!§7] §fThe config for the index §e" + index + "§f is empty, please read the document.");
+        }
+        else {
+
+            if (isClear) {
+                InventoryStorage.createInventoryStorage(player);
+                InventoryStorage storage = InventoryStorage.getInventoryStorage(player);
+                if (storage != null) {
+                    storage.getInventory().setContents(((PlayerInventory) player.getInventory()).getContents());
+
+                    Bukkit.getLogger().log(Level.INFO, "------------------------------------- 1");
+                    for (ItemStack item : storage.getInventory().getContents()) {
+                        if (item != null) {
+                            Bukkit.getLogger().log(Level.INFO, "Item: {0}", item);
+                        }
+                    }
+
+                    player.getInventory().clear();
+                    
+                    Bukkit.getLogger().log(Level.INFO, "------------------------------------- 2");
+                    for (ItemStack item : storage.getInventory().getContents()) {
+                        if (item != null) {
+                            Bukkit.getLogger().log(Level.INFO, "Item: {0}", item);
+                        }
+                    }
+                }
+            }
+
+            player.openInventory(getScreenInventory(player));
+        }
+    }
+}
